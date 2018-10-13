5b6c76175c7fa233dc6163421423a1f6b973a76c
Naoki Kinoshita <uttnaoki@gmail.com>
2018-10-13 15:36:44 +0900

    バス停を円でプロット

--- public/javascripts/gtfsmap.js
+++ public/javascripts/gtfsmap.js
@@ -1,6 +1,30 @@
-var mymap = L.map('mapid').setView([34.673716, 133.923387], 15);
+const mymap = L.map('mapid').setView([34.673716, 133.923387], 15);
 
 L.tileLayer(
   'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
   { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' }
 ).addTo(mymap);
+
+const geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+async function getStops(url) {
+  const response = await fetch(url);
+  console.log(response);
+  const json = await response.json();
+  console.log(json);
+
+  L.geoJSON(json, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+  }).addTo(mymap);
+}
+
+getStops('stops');
