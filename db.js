57bc24b140dc6771afeacad168b5c2a9b34387b3
Naoki Kinoshita <uttnaoki@gmail.com>
2018-12-11 14:30:34 +0900

    rm: db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const options = {
-  // initialization options;
-};
-
-const pgp = require("pg-promise")(options);
-const db = pgp("postgres://naoki@localhost:5432/gtfs-okayama");
-
-module.exports = db;
